abf99a30b76235a5b467e6744cb7cbde0948a955
Superechidna <30376296+Superechidna@users.noreply.github.com>
2018-09-27 11:37:04 -0400

    Removed unnecessary images and switched to building with sugarcube

--- javafunctions.js
+++
@@ -1,6 +0,0 @@
-$('.menu-item, #menu-core').hide();
-$('#menu-item-toggle-closed').show();
-$('#menu-item-toggle, #menu-item-toggle-closed')
-    .click( function () {
-            $('.menu-item, #menu-core').toggle(0);
-    });
